3f892f324a74b17e6e1ca5aad18b4d5ff8423629
harris-miller <harrismillerconsulting@gmail.com>
2018-06-22 04:23:25 -0600

    more react setup

--- bitcoin-testnet3-front-end/src/app.jsx
+++ bitcoin-testnet3-front-end/src/app.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './app.css';
+import React, { Component, Fragment } from 'react';
+import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import store from './store';
+import Intro from './scenes/intro';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Fragment>
+          <CssBaseline />
+          <Router>
+            <Route extact path="/" component={Intro} />
+          </Router>
+        </Fragment>
+      </Provider>
     );
   }
 }
--- bitcoin-testnet3-front-end/src/index.js
+++ bitcoin-testnet3-front-end/src/index.js
@@ -1,22 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import store from './store';
 import './index.css';
 import App from './app';
 import registerServiceWorker from './register-service-worker';
 
-const Root = () => (
-  <Provider store={store}>
-    <Fragment>
-      <CssBaseline />
-      <App />
-    </Fragment>
-  </Provider>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
