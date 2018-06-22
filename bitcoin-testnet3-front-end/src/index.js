3e3e0171e9df1aaa586b7cf12c06667a342379e0
harris-miller <harrismillerconsulting@gmail.com>
2018-06-22 04:02:44 -0600

    getting the react app setup

--- bitcoin-testnet3-front-end/src/index.js
+++ bitcoin-testnet3-front-end/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/* eslint-disable react/jsx-filename-extension */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import store from './store';
+import './index.css';
+import App from './app';
+import registerServiceWorker from './register-service-worker';
+
+const Root = () => (
+  <Provider store={store}>
+    <Fragment>
+      <CssBaseline />
+      <App />
+    </Fragment>
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
