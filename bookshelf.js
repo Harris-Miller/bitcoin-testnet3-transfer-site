f7127cdd87724a88c5cd057ad717c454493e4ebf
harris-miller <harrismillerconsulting@gmail.com>
2018-07-01 18:26:39 -0600

    added a turnary for bookshelf.js

--- bookshelf.js
+++ bookshelf.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const bookshelf = require('bookshelf');
 const knexConfig = require('./knexfile');
 
-const instance = bookshelf(knex(knexConfig.development));
+const instance = bookshelf(knex(process.env.NODE_ENV === 'production' ? knexConfig.production : knexConfig.development));
 instance.plugin('case-converter');
 
 module.exports = instance;
