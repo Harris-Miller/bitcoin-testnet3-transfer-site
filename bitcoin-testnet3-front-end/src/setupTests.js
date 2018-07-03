172cea71478701aadef1ff98178c91b8637c5a24
harris-miller <harrismillerconsulting@gmail.com>
2018-07-03 13:22:54 -0600

    some setup for testing

---
+++ bitcoin-testnet3-front-end/src/setupTests.js
@@ -0,0 +1,14 @@
+// this file is automatically read by `react-scripts test`
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+
+global.localStorage = localStorageMock
