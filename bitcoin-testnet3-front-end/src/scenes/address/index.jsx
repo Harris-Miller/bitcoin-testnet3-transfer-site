885355c95fc84f2adb77258eec5cbfc6fd3058cb
harris-miller <harrismillerconsulting@gmail.com>
2018-07-03 19:38:25 -0600

    redid the transaction progress. Looks way better

--- bitcoin-testnet3-front-end/src/scenes/address/index.jsx
+++ bitcoin-testnet3-front-end/src/scenes/address/index.jsx
@@ -6,7 +6,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import moment from 'moment';
 import AddressTitle from './title';
@@ -36,6 +36,10 @@ function sortTransactionsKeysByRecieved(txs) {
 }
 
 class Address extends Component {
+  progressPercentage = confirmations => ((confirmations >= 6) ? 100 : ((confirmations / 6) * 100));
+
+  progressColor = confirmations => (confirmations >= 6) ? 'primary' : 'secondary';
+
   withAddress(address) {
     const { classes } = this.props;
 
@@ -44,17 +48,21 @@ class Address extends Component {
 
       return (<ExpansionPanel key={txs.hash}>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography className={classes.heading}>{txs.hash}</Typography>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography className={classes.heading} gutterBottom={true}>{txs.hash}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <LinearProgress variant="buffer" color={this.progressColor(txs.confirmations)} value={this.progressPercentage(txs.confirmations)} />
+            </Grid>
+          </Grid>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Grid container justify="space-between" alignItems="center">
-            <Grid item>
-              <CircularProgress variant="static" value={txs.confirmations >= 6 ? 100 : ((txs.confirmations / 6) * 100)}/>
-            </Grid>
-            <Grid item>
+            <Grid item xs={12} md={6}>
               <BalanceDisplay text="Value" value={txsValue} />
             </Grid>
-            <Grid item>
+            <Grid item xs={12} md={6}>
               <Typography>Received: {txs.received && moment(txs.received).format('M/D/Y h:mm:ss a')}</Typography>
             </Grid>
           </Grid>
